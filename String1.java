c07e2bfed6a01982ca243a31390feccfaec29512
vishwakbalaji-a <vishwakbalaji2304@gmail.com>
2026-02-27 15:43:35 +0530

    modified

---
+++ String1.java
@@ -0,0 +1,10 @@
+public class String1 {
+    public static void main(String[] args) {
+        String str1 = "Hello World";
+        String str2 = "hello world";
+        String str3 = "   java programming";
+
+       System.out.println(str1);
+        
+    }
+}
