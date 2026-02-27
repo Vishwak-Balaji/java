fd755466c4dc0857b438ba9f7c61a64fa5af0e29
vishwakbalaji-a <vishwakbalaji2304@gmail.com>
2026-02-27 13:53:28 +0530

    ggg

---
+++ variables.java
@@ -0,0 +1,11 @@
+public class variables{
+    public static void main(String[] args) {
+        byte a = 10;
+        short b = 20;
+        int c = 30;
+        long d = 50000;
+        double e = 20.0;
+        char j ='h';
+        boolean k =true;
+    }
+}
