d16c853eed4647ab3e4561b4038deacb47dc9e69
vishwakbalaji-a <vishwakbalaji2304@gmail.com>
2026-03-02 11:05:38 +0530

    example for break and continue

--- Break_and_Continue.java
+++ Break_and_Continue.java
@@ -1,1 +1,19 @@
-
+import java.util.*;
+ public class Break_and_Continue{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i =1;i<=n;i++){
+            if(i==5){
+                break;
+            }
+            System.out.println(i);
+        }
+        for(int i=1;i<=n;i++){
+            if(i%2!=0){
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
