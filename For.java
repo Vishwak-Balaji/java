3e4b557ac19d89d0d5ba23f7ec240bf249661e92
vishwakbalaji-a <vishwakbalaji2304@gmail.com>
2026-03-02 10:48:29 +0530

    updated for example

--- For.java
+++ For.java
@@ -1,1 +1,13 @@
+import java.util.Scanner;
+public class For{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int n = sc.nextInt();
+        for(int i=1;i<=10;i++){
+            System.out.println(n+"X"+i+"="+n*i);
+        }
 
+
+    }
+}
