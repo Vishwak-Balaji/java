3c9fa4ff79552dacc4b80672673f8a2a7c64c9a6
vishwakbalaji-a <vishwakbalaji2304@gmail.com>
2026-03-02 10:09:56 +0530

    updated switch

--- Switch.java
+++ Switch.java
@@ -1,1 +1,38 @@
 
+import java.util.Scanner;
+
+public class Switch{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the firet number");
+        int a = sc.nextInt();
+        System.out.println("Enter the secod number");
+        int b = sc.nextInt();
+        System.out.println("Enter te operator");
+        char c = sc.next().charAt(0);
+
+        switch(c){
+            case '+':
+                System.out.println("Result = " + (a + b));
+                break;
+
+            case '-':
+                System.out.println("Result = " + (a - b));
+                break;
+
+            case '*':
+                System.out.println("Result = " + (a * b));
+                break;
+
+            case '/':
+                if(b != 0)
+                    System.out.println("Result = " + (a / b));
+                else
+                    System.out.println("Cannot divide by zero");
+                break;
+            default:
+                System.out.println("invalid operator");       
+        }
+        sc.close();
+    }
+}
