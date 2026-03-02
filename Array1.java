f11e4dcd23f56f319f306d1692c37fdbb5775e7c
vishwakbalaji-a <vishwakbalaji2304@gmail.com>
2026-03-02 12:32:49 +0530

    Array example

--- Array1.java
+++ Array1.java
@@ -1,1 +1,21 @@
+import java.util.*;
 
+public class Array1{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the array size");
+        int n= sc.nextInt();
+        int [] arr = new int[n];
+        System.out.println("enter the array elements");
+        for(int i = 0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int max =arr[0];
+        for(int num:arr){
+            if(num>max){
+                max=num;
+            }
+        }
+        System.out.println("largest number is :"+ max);
+    }
+}
