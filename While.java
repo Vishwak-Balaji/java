b2e31bdf84e0ef27bfc4d5021ea7a979634fafa2
vishwakbalaji-a <vishwakbalaji2304@gmail.com>
2026-03-02 10:24:45 +0530

    while code uodated

--- While.java
+++ While.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
 
+public class While{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int i = sc.nextInt();
+
+        int reverse =0;
+        while(i!=0){
+            int digit = i%10;
+            reverse=reverse*10+digit;
+            i=i/10;
+
+        }
+        System.out.println(reverse);
+    }
+}
