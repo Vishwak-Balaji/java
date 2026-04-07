f88c99c04586c5be64a4ed298cf67bab7602dab5
vishwakbalaji-a <vishwakbalaji2304@gmail.com>
2026-04-07 17:16:23 +0530

    for ech method

---
+++ ForEachMethod.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+
+public class ForEachMethod {
+    public static void main(String[] args) {
+        List<Integer>list = Arrays.asList(1,2,3,4,5,6);
+        list.forEach(n->System.out.println(n));
+    }
+}
