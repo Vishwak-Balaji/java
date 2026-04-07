dac75f356a1c59e53fe2d51c29748a15071212c3
vishwakbalaji-a <vishwakbalaji2304@gmail.com>
2026-04-07 15:15:36 +0530

    updated

---
+++ AbstractAnonymousInnerClass.java
@@ -0,0 +1,13 @@
+public class AbstractAnonymousInnerClass {
+    public static void main(String[] args) {
+        Demo obj = new Demo(){
+            public void display(){
+                System.out.println("hello world");
+            }
+        };
+        obj.display();
+    }
+}
+abstract class Demo{
+    public abstract  void display();
+}
---
+++ AnonymousInnerClass.java
@@ -0,0 +1,15 @@
+public class AnonymousInnerClass {
+    public static void main(String[] args) {
+        A obj = new A(){
+            public void show(){
+                System.out.println("Iam changed..!");
+            }
+        };
+        obj.show();
+    }
+}
+class A{
+    public void show(){
+        System.out.println("change me");
+    }
+}
